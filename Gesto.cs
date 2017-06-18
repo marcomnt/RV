2b8a188a3beda0678402896e3e4934f562e869a7
marcomnt <mpmr@cin.ufpe.br>
2017-06-18 19:23:49 -0300

    Add files via upload

---
+++ Gesto.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gesto{
+    LeapGesture gestoLeap;
+    KinnectGesture gestoKinnect;
+
+    // Use this for initialization
+    public Gesto () {
+        gestoLeap = new LeapGesture();
+        gestoKinnect = new KinnectGesture();
+	} 
+
+    public void captura() {
+        gestoLeap.capturarLeap();
+        gestoKinnect.capturarKinnect();
+    }
+    public bool ended() {
+        return gestoLeap.is_finalizated();
+    }
+    public void set_gesto(Gesto g) {
+        gestoKinnect = g.get_KinnectGesture();
+        gestoLeap = g.get_LeapGesture();
+    }
+
+    private KinnectGesture get_KinnectGesture(){
+        return gestoKinnect;
+    }
+    private LeapGesture get_LeapGesture()
+    {
+        return gestoLeap;
+    }
+}
---
+++ ativ.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ativ : MonoBehaviour {
+    private GameObject objeto;
+    public bool ativo=false;
+	// Use this for initialization
+	void Start () {
+        objeto = GameObject.Find("BasicFire");
+        if (objeto!= null)
+        {
+            //Debug.Log("ok");
+        }
+        else
+        {
+            Debug.Log("OPS");
+        }
+        objeto.SetActive(ativo);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    public void on()
+    {
+        ativo = true;
+        objeto.SetActive(ativo);
+    }
+
+    public void off()
+    {
+        ativo = false;
+        objeto.SetActive(ativo);
+    }
+}
