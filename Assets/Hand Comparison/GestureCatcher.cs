15c5dd62f58ffea9a8017ce8f4f452267015976a
Renato Deyvson <renatodeyvson@gmail.com>
2017-06-26 21:52:54 -0300

    Hand comparison

---
+++ Assets/Hand Comparison/GestureCatcher.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap.Unity;
+
+public class GestureCatcher : MonoBehaviour {
+
+    RigidHand[] hands;
+    // Use this for initialization
+    void Start () {
+       RigidHand[] hands = (RigidHand[])GameObject.FindObjectsOfType(typeof(RigidHand));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		foreach(RigidHand hand in hands)
+        {
+            new LeapHand(hand);
+        }
+    }
+
+}
